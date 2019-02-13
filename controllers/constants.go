8aed8940b0e8df56363d840d6ab4fc73c76088fa
Michael <michael@localhost.localdomain>
2019-02-13 12:20:44 -0500

    dev env

--- controllers/constants.go
+++ controllers/constants.go
@@ -1,13 +1,13 @@
 package controllers
 
+const Environment = "dev" //checks for prod, anything else starts a normal server
+
 const AsanaBase = "https://app.asana.com/api/1.0"
 const SupportProjectID = "1107602815071855"
 const UrgentTagGid = "1107602815071859"
 const NewTagGid = "1107602815071860"
 const PendingTagGid = "1107602815071861"
 const ResolvedTagGid = "1107602815071862"
-
-const Environment = "prod" //checks for prod, anything else starts a normal server
 
 const TwilioNumber = "+18594465953"
 const TwiML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
