8163ad103647b61d75e3fa63a5f6f683eaebc6ae
Michael <michael@localhost.localdomain>
2019-05-03 15:56:12 -0400

    fixed file

--- models/folks.go
+++ models/folks.go
@@ -4,10 +4,7 @@ type SupportAgent struct {
 	Email string `json:"email"`
 	Phone string `json:"phone"`
 }
-<<<<<<< HEAD
-=======
 
 type Folks struct {
 	Agents []SupportAgent `json:"data"`
 }
->>>>>>> development
